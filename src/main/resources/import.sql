f91660e5c60b0c5bd97d73e28cd9b3a19e65ad14
kboets <k.boets@gmail.com>
2017-01-08 06:15:22 +0100

    changed from datasource : Oracle XE instead H2 + using flyway for database setup : no longer needed import.sql

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into ADDRESS (STREET, NUMBER, ZIPCODE, CITY) VALUES  ('Westelsebaan', '175', '3271', 'Averbode');
-
-insert into PERSON (LAST_NAME, FIRST_NAME, PERSON_ADDRESS_ID) VALUES  ('Jansens', 'Kurt', 1);
-insert into PERSON (LAST_NAME, FIRST_NAME, PERSON_ADDRESS_ID) VALUES  ('Devis', 'Els', 1);
-insert into PERSON (LAST_NAME, FIRST_NAME, PERSON_ADDRESS_ID) VALUES  ('Jansens', 'Jorre', 1);
-insert into PERSON (LAST_NAME, FIRST_NAME, PERSON_ADDRESS_ID) VALUES  ('Jansens', 'Féline', 1);
