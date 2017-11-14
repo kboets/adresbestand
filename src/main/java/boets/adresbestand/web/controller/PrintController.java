b40a08bf7a2b723642f0c3aa1ed851a5a079aea8
kboets <k.boets@gmail.com>
2017-11-14 21:33:46 +0100

    working version open pdf singlePerson

--- src/main/java/boets/adresbestand/web/controller/PrintController.java
+++ src/main/java/boets/adresbestand/web/controller/PrintController.java
@@ -1,30 +1,18 @@
 package boets.adresbestand.web.controller;
 
-import boets.adresbestand.domain.Person;
 import boets.adresbestand.service.IPrintService;
-import org.apache.pdfbox.pdmodel.PDDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by Asus on 4/11/2017.
@@ -34,21 +22,19 @@ public class PrintController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private static final String FILE_PATH = "D:\\tmp\\pdf-sample.pdf";
-
     @Autowired
     private IPrintService printService;
 
     @GetMapping("/print/{personId}")
-    public ResponseEntity<ByteArrayResource> printAddress(@PathVariable Long personId) throws IOException {
+    public ResponseEntity<byte[]> printAddress(@PathVariable Long personId) throws IOException {
         byte[] data = printService.createPdf(personId);
-
-        ByteArrayResource resource = new ByteArrayResource(data);
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment;filename=" + "adresbestand.pdf")
-                .contentType(MediaType.APPLICATION_PDF).contentLength(data.length)
-                .body(resource);
+        HttpHeaders headers = new HttpHeaders();
+        String fileName = "adresbestand.pdf";
+        headers.add("content-disposition", "inline;filename=" + fileName);
+        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(
+                data, headers, HttpStatus.OK);
+        return response;
 
     }
 
