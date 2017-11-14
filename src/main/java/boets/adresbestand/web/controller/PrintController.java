7bcd4996134cb15e10ca3a4e3f608762ea4341ee
kboets <k.boets@gmail.com>
2017-11-14 19:52:49 +0100

    working version download default pdf

--- src/main/java/boets/adresbestand/web/controller/PrintController.java
+++ src/main/java/boets/adresbestand/web/controller/PrintController.java
@@ -6,6 +6,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,39 +33,50 @@ import java.util.Optional;
 public class PrintController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static final String FILE_PATH = "D:\\tmp\\pdf-sample.pdf";
+
     @Autowired
     private IPrintService printService;
 
     @GetMapping("/print")
-    public ResponseEntity<byte[]> printAddress(Model model, @ModelAttribute("person") Person person) {
+    public ResponseEntity<ByteArrayResource> printAddress() throws IOException {
         List<Person> personList = new ArrayList<>();
-        personList.add(person);
-        return printAddress(personList);
+        Path path = Paths.get(FILE_PATH);
+        byte[] data = Files.readAllBytes(path);
+
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename=" + path.getFileName().toString())
+                .contentType(MediaType.APPLICATION_PDF).contentLength(data.length)
+                .body(resource);
+
     }
 
-    @GetMapping("/print}")
-    public ResponseEntity<byte[]> printAddress(@PathVariable List<Person> persons) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType("application/pdf"));
-        String filename = "output.pdf";
-        headers.setContentDispositionFormData(filename, filename);
-        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        ResponseEntity<byte[]> response = null;
-
-        Optional<PDDocument> documentOptional = printService.createPdf(persons);
-        if(documentOptional.isPresent()){
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            try {
-                documentOptional.get().save(byteArrayOutputStream);
-                documentOptional.get().close();
-                response = new ResponseEntity<byte[]>(byteArrayOutputStream.toByteArray(), headers, HttpStatus.OK);
-            } catch (IOException e) {
-                logger.warn(e.getCause().getMessage());
-                byte[] errorBytes = new byte[]{};
-                return new ResponseEntity<byte[]>(errorBytes,headers,HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return response;
-    }
+//    @GetMapping("/print}")
+//    public ResponseEntity<byte[]> printAddress(@PathVariable List<Person> persons) {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+//        String filename = "output.pdf";
+//        headers.setContentDispositionFormData(filename, filename);
+//        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+//        ResponseEntity<byte[]> response = null;
+//
+//        Optional<PDDocument> documentOptional = printService.createPdf(persons);
+//        if(documentOptional.isPresent()){
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//            try {
+//                documentOptional.get().save(byteArrayOutputStream);
+//                documentOptional.get().close();
+//                response = new ResponseEntity<byte[]>(byteArrayOutputStream.toByteArray(), headers, HttpStatus.OK);
+//            } catch (IOException e) {
+//                logger.warn(e.getCause().getMessage());
+//                byte[] errorBytes = new byte[]{};
+//                return new ResponseEntity<byte[]>(errorBytes,headers,HttpStatus.INTERNAL_SERVER_ERROR);
+//            }
+//        }
+//
+//        return response;
+//    }
 }
