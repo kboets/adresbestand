faadac37e2f86694b590b43592e60ceaa0144e48
kboets <k.boets@gmail.com>
2018-10-08 17:38:34 +0200

    use angular as search

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -2,8 +2,12 @@ adresbestand.controller('mainController', ['$scope','_', function ($scope, _) {
     $scope.persons = [];
     $scope.checkedPersons = [];
 
+    $scope.searchObject = {
+        firstName:'',
+        lastName:''
+    };
     $scope.runSearch = function (searchObject) {
-        if(_.isEmptyObject(searchObject)){
+        if(searchObject.firstName === '' && searchObject.lastName === ''){
             $scope.searchAddressForm.$invalid = true;
             return false;
         }
