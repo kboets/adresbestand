aa1d7e2bc844f1926cc3a3faa05a0c09e75ab2c4
kboets <k.boets@gmail.com>
2019-01-12 08:29:15 +0100

    added new Karma test + upgraded pom to 1.6

--- src/test/angular/controllerTest.js
+++ src/test/angular/controllerTest.js
@@ -2,15 +2,15 @@ describe('Container file for all controller tests of adresbestand application', 
 
     describe('searchController', function () {
 
-        var ctrl, $scope, q;
+        var ctrl, $scope, q, httpBackend;
 
         beforeEach(function () {
             module("adresbestand");
 
-            inject(function ($controller, $rootScope, $injector) {
+            inject(function ($controller, $rootScope, $injector, $httpBackend) {
                 $scope = $rootScope.$new();
                 q = $injector.get('$q');
-
+                httpBackend = $httpBackend;
                 ctrl = mockito4js.spy($controller('searchController', {
                     $scope: $scope
                 }));
@@ -38,7 +38,44 @@ describe('Container file for all controller tests of adresbestand application', 
 
         });
 
+        it('Should return all persons when selecting searchAll', function () {
+            httpBackend.expectGET("/findAll").respond({
+                allPersons : [{
+                    "firstName": "Kurt",
+                    "lastName": "Boets"
+                },{
+                    "firstName": "Jorre",
+                    "lastName": "Boets"
+                }],
+                searched :true
+            })
+            expect($scope.persons).toEqual[0];
+            expect($scope.searched).toBe(false);
+            $scope.searchAll();
+            expect($scope.persons).toEqual[2];
+            expect($scope.searched).toBe(true);
+        });
+
+        it('Should return correct person(s) with select criteria when calling searchAddress', function () {
+            $scope.searchObject = {
+                firstName:'Kurt',
+                lastName:'Boets'
+            };
+            httpBackend.expectPOST("/searchAddress").respond({
+                person : [{
+                    "firstName": "Kurt",
+                    "lastName": "Boets"
+                }]
+            });
+
+            expect($scope.persons).toEqual[0];
+            $scope.searchPersons($scope.searchObject);
+            expect($scope.persons).toEqual[1];
+
+        });
     });
+
+
 });
 
 
